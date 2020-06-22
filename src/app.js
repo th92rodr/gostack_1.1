d468e50fff766cf7d3454aac98cc0096e879c079
th92rodr <thiago.clrodrigues@gmail.com>
2020-06-22 17:02:37 -0300

    add log-requests and validate-repository-id middlewares

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("./routes");
+const { logRequests } = require("./middleware");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(logRequests);
 app.use(routes);
 
 module.exports = app;
---
+++ src/middleware.js
@@ -0,0 +1,31 @@
+const { isUuid } = require("uuidv4");
+
+const { repositories } = require("./controller");
+
+const logRequests = (request, response, next) => {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+  return next();
+};
+
+const validateRepositoryId = (request, response, next) => {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid repository ID." });
+  }
+
+  const repositoryIndex = repositories.findIndex((r) => r.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+  return next();
+};
+
+module.exports = {
+  logRequests,
+  validateRepositoryId,
+};
